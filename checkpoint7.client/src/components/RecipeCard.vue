452c6a1eb05440b25dfb201f1b3626408edd5625
Zach Rasmussen <zsrasmussen@gmail.com>
2022-10-10 17:02:34 -0600

    working on creating dynamic recipe card

--- checkpoint7.client/src/components/RecipeCard.vue
+++ checkpoint7.client/src/components/RecipeCard.vue
@@ -10,7 +10,7 @@
   >
     <!-- SECTION this is the template for the recipe cards -->
     <!-- NOTE to action the modal for the recipe details is in the image -->
-    <div class="col-10 col-md-4 recipe-item mb-5">
+    <div class="col-10 col-md-12 recipe-item mb-5">
       <img
         src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80"
         alt=""
@@ -29,7 +29,7 @@
     </div>
 
     <!-- SECTION end of the template section -->
-    <div class="col-10 col-md-4 recipe-item mb-5">
+    <!-- <div class="col-10 col-md-4 recipe-item mb-5">
       <img
         src="https://images.unsplash.com/photo-1524334788144-6dc88da21500?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
         alt=""
@@ -54,7 +54,7 @@
           So good a rabbit would eat it!
         </p>
       </div>
-    </div>
+    </div> -->
     <div class="col-12">
       <RecipeDetails />
     </div>
